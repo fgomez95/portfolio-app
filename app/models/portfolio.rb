f7feff5c94f077c036499e5eb34fcf9c468ba5e6
Fernando G <kev9504@gmail.com>
2019-01-29 15:35:09 +0000

    implemented defaults to portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,15 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+    after_initialize :set_defaults
     
     scope :first_five, ->{ last(5).reverse }
     
     def self.first_two
         last(2).reverse
+    end
+    
+    def set_defaults
+        self.main_image ||= "https://placehold.it/600x400"
+        self.thumb_image ||= "https://placehold.it/350x200"
     end
 end
